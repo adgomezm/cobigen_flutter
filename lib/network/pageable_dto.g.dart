83c47c846c202d33cae20aed0515de1fec804a27
adgomezm <adam.gomez-mouti-external@capgemini.com>
2021-07-09 11:43:39 +0200

    json generados del pageable

---
+++ lib/network/pageable_dto.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'pageable_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PageableDto _$PageableDtoFromJson(Map<String, dynamic> json) {
+  return PageableDto(
+    json['pageSize'] as int,
+    json['pageNumber'] as int,
+    json['sort'] as List,
+  );
+}
+
+Map<String, dynamic> _$PageableDtoToJson(PageableDto instance) =>
+    <String, dynamic>{
+      'pageSize': instance.pageSize,
+      'pageNumber': instance.pageNumber,
+      'sort': instance.sort,
+    };
